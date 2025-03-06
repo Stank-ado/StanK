0e5bfa392b44aec69a6fbf4a2f4d68620b10d992
Stank-ado <munozrogeledgarandres2@gmail.com>
2025-03-05 22:01:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Lista de imágenes
-const imagenes = ["miniatura2.png", "estandarte.png"];
+// Lista de imágenes y sus descripciones
+const imagenes = [
+    { src: "miniatura2.png", texto: "miniatura del canal que nunca cree" },
+    { src: "estandarte.png", texto: "estandarte del canal que nunca cree jejeje" },
+    { src: "logo.png", texto: "logo" },
+
+    
+];
+
 let indice = 0;
 
 function cambiarImagen() {
-    // Obtener la imagen
+    // Obtener la imagen y el texto
     let img = document.getElementById("imagen");
+    let descripcion = document.getElementById("descripcion");
 
     // Cambiar al siguiente índice
     indice = (indice + 1) % imagenes.length;
 
-    // Actualizar la imagen
-    img.src = imagenes[indice];
+    // Actualizar la imagen y el texto
+    img.src = imagenes[indice].src;
+    descripcion.textContent = imagenes[indice].texto;
 }
